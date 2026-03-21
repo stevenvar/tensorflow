e9e225cb688a9bfd0d12f8d2ce12fbf24f5c85f5
Steven Varoumas <steven.varoumas1@huawei.com>
2026-03-21 13:46:50 +0000

    Fix review nits in XLA CPU plumbing

--- third_party/xla/xla/service/reduce_scatter_combiner.cc
+++ third_party/xla/xla/service/reduce_scatter_combiner.cc
@@ -131,9 +131,9 @@ absl::Status CombineReduceScatters(
       std::swap((*perm)[most_frequent_dim], (*perm)[rs->scatter_dimension()]);
 
       // Bitcast operand and update output shape.
-      auto sh = ShapeUtil::PermuteDimensions(*perm, operand_shape), operand;
-      operands.back() =
-          computation.AddInstruction(HloInstruction::CreateBitcast(sh));
+      auto sh = ShapeUtil::PermuteDimensions(*perm, operand_shape);
+      operands.back() = computation.AddInstruction(
+          HloInstruction::CreateBitcast(sh, operand));
       output_shapes.back() = ShapeUtil::PermuteDimensions(*perm, hlo->shape());
     }
   }

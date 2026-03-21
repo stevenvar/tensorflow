88f7e31e6e1fda1af704bca166cae77ff29aed61
Steven Varoumas <steven.varoumas1@huawei.com>
2026-03-21 20:29:02 +0000

    Remove dead CPU-only symbolic passes

--- third_party/xla/xla/service/cpu/cpu_compiler.cc
+++ third_party/xla/xla/service/cpu/cpu_compiler.cc
@@ -152,8 +152,6 @@ limitations under the License.
 #include "xla/map_util.h"
 #include "xla/mlir_hlo/transforms/passes.h"
 #include "xla/service/all_reduce_promotion.h"
-#include "xla/service/outer_dimension_propagation.h"
-#include "xla/service/get_outer_batch_value_simplifier.h"
 #include "xla/service/all_to_all_decomposer.h"
 #include "xla/service/batched_gather_scatter_normalizer.h"
 #include "xla/service/batchnorm_expander.h"
@@ -929,8 +927,6 @@ absl::Status CpuCompiler::RunHloPassesAfterLayoutAssn(
     pipeline.AddPass<SmallWhileLoopHoistingPass>(byte_threshold);
   }
 
-  // pipeline.AddPass<OuterDimensionPropagationPass>();
-  // pipeline.AddPass<GetOuterBatchValueSimplifier>();
   pipeline.AddPass<HloDCE>();
   return pipeline.Run(module).status();
 }

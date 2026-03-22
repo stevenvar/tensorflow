f8ac64d9fd0fbafc7a5449155941118c973a562f
Steven Varoumas <steven.varoumas1@huawei.com>
2026-03-22 08:31:02 +0000

    Remove stray CPU compiler conflict markers

--- third_party/xla/xla/service/cpu/cpu_compiler.cc
+++ third_party/xla/xla/service/cpu/cpu_compiler.cc
@@ -152,12 +152,7 @@ limitations under the License.
 #include "xla/map_util.h"
 #include "xla/mlir_hlo/transforms/passes.h"
 #include "xla/service/all_reduce_promotion.h"
-<<<<<<< HEAD
-=======
 #include "xla/service/dynamic_constant_rewriter.h"
-#include "xla/service/get_expression_value_simplifier.h"
-#include "xla/service/outer_dimension_propagation.h"
->>>>>>> f6c963e2683 (Add HLO pass to rewrite marked dynamic constants)
 #include "xla/service/all_to_all_decomposer.h"
 #include "xla/service/batched_gather_scatter_normalizer.h"
 #include "xla/service/batchnorm_expander.h"

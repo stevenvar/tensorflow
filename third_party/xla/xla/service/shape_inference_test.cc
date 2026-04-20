c84b2cbbd0534babf9952456b319b28a2b288d42
Steven Varoumas <steven.varoumas1@huawei.com>
2026-04-20 20:52:35 +0100

    Check dimension expressions in XLA shape inference

--- third_party/xla/xla/service/shape_inference_test.cc
+++ third_party/xla/xla/service/shape_inference_test.cc
@@ -2822,6 +2822,18 @@ TEST_F(ShapeInferenceTest, BinOpBroadcastBadDimension) {
               HasSubstr("dimensions order is wrong"));
 }
 
+TEST_F(ShapeInferenceTest, BinOpPreservesBroadcastedExpressionSameRank) {
+  Shape lhs = ShapeUtil::MakeShape(F32, {1}, {true});
+  lhs.set_expression(0, DExpr::Const(1));
+  Shape rhs = ShapeUtil::MakeShape(F32, {1}, {true});
+  rhs.set_expression(0, DExpr::Var(1));
+
+  const absl::StatusOr<Shape> inferred_shape =
+      ShapeInference::InferBinaryOpShape(HloOpcode::kAdd, lhs, rhs, {0});
+  ASSERT_IS_OK(inferred_shape.status());
+  EXPECT_EQ(inferred_shape->expressions(0), DExpr::Var(1));
+}
+
 // Tests for the while instruction with proper shapes.
 TEST_F(ShapeInferenceTest, WhileWithCorrectShapes) {
   const Shape result_shape = ShapeUtil::MakeTupleShape({s32_, vector_32_});

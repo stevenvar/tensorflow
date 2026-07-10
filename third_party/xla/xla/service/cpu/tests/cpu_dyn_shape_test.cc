90aa94d63e00e2c9fc1a5092bf576fb2ef0ef1b7
Guillermo-Callaghan <guillermo.callaghan@huawei.com>
2026-07-10 15:57:04 +0100

    Add CPU fast concatenate dynamic-shape regression tests

--- third_party/xla/xla/service/cpu/tests/cpu_dyn_shape_test.cc
+++ third_party/xla/xla/service/cpu/tests/cpu_dyn_shape_test.cc
@@ -18,6 +18,8 @@ limitations under the License.
 #include <utility>
 
 #include <gtest/gtest.h>
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
 #include "xla/hlo/ir/hlo_computation.h"
 #include "xla/hlo/ir/hlo_instruction.h"
 #include "xla/hlo/ir/hlo_opcode.h"
@@ -32,7 +34,68 @@ namespace xla {
 namespace cpu {
 namespace {
 
-using CpuDynamicShapeTest = CpuCodegenTest;
+CpuAotCompilationOptions GetAotOptions() {
+  return CpuAotCompilationOptions{
+      /*triple=*/kTargetTripleForHost, /*cpu_name=*/kTargetCpuForHost,
+      /*features=*/"",
+      /*entry_point_name=*/"entry",
+      /*relocation_model=*/CpuAotCompilationOptions::RelocationModel::Static};
+}
+
+Shape MakeDynamicDenseShape(absl::Span<const int64_t> dimensions,
+                            absl::Span<const int64_t> minor_to_major,
+                            int64_t dynamic_dimension) {
+  Shape shape =
+      ShapeUtil::MakeShapeWithDenseLayout(F32, dimensions, minor_to_major);
+  shape.set_dynamic_dimension(dynamic_dimension, true);
+  return shape;
+}
+
+class CpuDynamicShapeTest : public CpuCodegenTest {
+ protected:
+  std::unique_ptr<HloModule> CreateDynamicFastConcatModule(
+      absl::string_view name, bool add_consumer, bool three_operands = false) {
+    HloComputation::Builder builder(name);
+
+    Shape operand_shape = MakeDynamicDenseShape({64, 26, 4}, {2, 1, 0},
+                                                /*dynamic_dimension=*/0);
+    Shape concat_shape =
+        MakeDynamicDenseShape({64, 26, three_operands ? 12 : 8}, {2, 1, 0},
+                              /*dynamic_dimension=*/0);
+
+    HloInstruction* lhs = builder.AddInstruction(
+        HloInstruction::CreateParameter(0, operand_shape, "lhs"));
+    HloInstruction* rhs = builder.AddInstruction(
+        HloInstruction::CreateParameter(1, operand_shape, "rhs"));
+
+    HloInstruction* concat = nullptr;
+    if (three_operands) {
+      HloInstruction* third = builder.AddInstruction(
+          HloInstruction::CreateParameter(2, operand_shape, "third"));
+      concat = builder.AddInstruction(HloInstruction::CreateConcatenate(
+          concat_shape, {lhs, rhs, third}, /*dimension=*/2));
+    } else {
+      concat = builder.AddInstruction(HloInstruction::CreateConcatenate(
+          concat_shape, {lhs, rhs}, /*dimension=*/2));
+    }
+
+    if (add_consumer) {
+      int64_t addend_param_number = three_operands ? 3 : 2;
+      HloInstruction* addend =
+          builder.AddInstruction(HloInstruction::CreateParameter(
+              addend_param_number, concat_shape, "addend"));
+      builder.AddInstruction(HloInstruction::CreateBinary(
+          concat_shape, HloOpcode::kAdd, concat, addend));
+    }
+
+    auto hlo_module = CreateNewVerifiedModule();
+    hlo_module->AddEntryComputation(builder.Build());
+    auto& debug_options = hlo_module->mutable_config().mutable_debug_options();
+    debug_options.set_xla_cpu_use_thunk_runtime(false);
+    debug_options.add_xla_disable_hlo_passes("fusion");
+    return hlo_module;
+  }
+};
 
 TEST_F(CpuDynamicShapeTest, DynamicShapeR2) {
   HloComputation::Builder builder(TestName());
@@ -66,6 +129,56 @@ TEST_F(CpuDynamicShapeTest, DynamicShapeR2) {
       .set_xla_cpu_use_thunk_runtime(false);
 
   CompileAheadOfTimeAndVerifyIr(std::move(hlo_module), options,
+                                filecheck_pattern,
+                                /*match_optimized_ir=*/false);
+}
+
+TEST_F(CpuDynamicShapeTest, DynamicFastConcatenateCompilesWithoutFusion) {
+  auto hlo_module =
+      CreateDynamicFastConcatModule(TestName(), /*add_consumer=*/false);
+
+  std::string filecheck_pattern = R"(
+; CHECK: %[[dyn_dim_size:.*]] = load i32, ptr
+; CHECK: %[[i64_dyn_dim_size:.*]] = sext i32 %[[dyn_dim_size:.*]] to i64
+; CHECK: br label %concatenate.loop_header.concat.1
+; CHECK: call void @llvm.memcpy
+  )";
+
+  CompileAheadOfTimeAndVerifyIr(std::move(hlo_module), GetAotOptions(),
+                                filecheck_pattern,
+                                /*match_optimized_ir=*/false);
+}
+
+TEST_F(CpuDynamicShapeTest,
+       DynamicFastConcatenateWithAddCompilesWithoutFusion) {
+  auto hlo_module =
+      CreateDynamicFastConcatModule(TestName(), /*add_consumer=*/true);
+
+  std::string filecheck_pattern = R"(
+; CHECK: %[[dyn_dim_size:.*]] = load i32, ptr
+; CHECK: %[[i64_dyn_dim_size:.*]] = sext i32 %[[dyn_dim_size:.*]] to i64
+; CHECK: br label %concatenate.loop_header.concat.1
+; CHECK: call void @llvm.memcpy
+  )";
+
+  CompileAheadOfTimeAndVerifyIr(std::move(hlo_module), GetAotOptions(),
+                                filecheck_pattern,
+                                /*match_optimized_ir=*/false);
+}
+
+TEST_F(CpuDynamicShapeTest,
+       DynamicFastConcatenateWithThreeOperandsCompilesWithoutFusion) {
+  auto hlo_module = CreateDynamicFastConcatModule(
+      TestName(), /*add_consumer=*/true, /*three_operands=*/true);
+
+  std::string filecheck_pattern = R"(
+; CHECK: %[[dyn_dim_size:.*]] = load i32, ptr
+; CHECK: %[[i64_dyn_dim_size:.*]] = sext i32 %[[dyn_dim_size:.*]] to i64
+; CHECK: br label %concatenate.loop_header.concat.1
+; CHECK: call void @llvm.memcpy
+  )";
+
+  CompileAheadOfTimeAndVerifyIr(std::move(hlo_module), GetAotOptions(),
                                 filecheck_pattern,
                                 /*match_optimized_ir=*/false);
 }

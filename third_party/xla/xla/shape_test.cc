d8b93c9867c0f3c18d3202844e3eaf3b58e8c7e0
Steven Varoumas <steven.varoumas1@huawei.com>
2026-07-28 15:14:41 +0100

    Fix dynamic Shape test construction

--- third_party/xla/xla/shape_test.cc
+++ third_party/xla/xla/shape_test.cc
@@ -53,9 +53,10 @@ class ShapeTest : public ::testing::Test {
   const Shape nested_tuple_ =
       ShapeUtil::MakeTupleShape({tuple_, matrix_, token_});
   const Shape dynamic_matrix_ =
-      ShapeUtil::MakeShape(S32, {5, 2}, {true, false});
+      ShapeUtil::MakeShape(S32, {5, 2}, std::vector<bool>{true, false}, {});
   const Shape unbounded_ =
-      ShapeUtil::MakeShape(F32, {Shape::kUnboundedSize, 784}, {true, false});
+      ShapeUtil::MakeShape(F32, {Shape::kUnboundedSize, 784},
+                           std::vector<bool>{true, false}, {});
 };
 
 // Tests that if the dynamic_dimensions parameter empty in the Shape
@@ -105,8 +106,8 @@ TEST_F(ShapeTest, ShapeToString) {
 }
 
 TEST_F(ShapeTest, DynamicShapeToString) {
-  Shape array_shape =
-      ShapeUtil::MakeShape(F32, {23, 44, 55}, {true, false, true});
+  Shape array_shape = ShapeUtil::MakeShape(
+      F32, {23, 44, 55}, std::vector<bool>{true, false, true}, {});
   EXPECT_EQ("f32[<=23,44,<=55]", array_shape.ToString());
 
   array_shape.set_dynamic_dimension(2, false);
